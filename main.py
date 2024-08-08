b4b74a652f25a70a5c6ee1792d4c54ca94efef55
urasefe <162623038+urasefe@users.noreply.github.com>
2024-08-08 13:49:31 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,45 @@
-import random
-
-characters = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-
-a = int(input("parola için belirli bir uzunluk belirleyin"))
-b = ""
+# Import / İçe Aktarma
+from flask import Flask, render_template
 
 
-for i in range(a):
-    b += random.choice(characters)
+app = Flask(__name__)
 
-print("şifreniz", b)
+def result_calculate(size, lights, device):
+    # Elektrikli cihazların enerji tüketimini hesaplamaya olanak tanıyan değişkenler
+    home_coef = 100
+    light_coef = 0.04
+    devices_coef = 5   
+    return size * home_coef + lights * light_coef + device * devices_coef 
+
+# İlk sayfa
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# İkinci sayfa
+@app.route('/<size>')
+def lights(size):
+    return render_template(
+                            'lights.html', 
+                            size=size
+                           )
+
+# Üçüncü sayfa
+@app.route('/<size>/<lights>')
+def electronics(size, lights):
+    return render_template(
+                            'electronics.html',
+                            size = size, 
+                            lights = lights                           
+                           )
+
+# Hesaplama
+@app.route('/<size>/<lights>/<device>')
+def end(size, lights, device):
+    return render_template('end.html', 
+                            result=result_calculate(int(size),
+                                                    int(lights), 
+                                                    int(device)
+                                                    )
+                        )
+app.run(debug=True)
