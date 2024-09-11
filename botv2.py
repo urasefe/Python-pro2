eff0e737f737ca1cfa193e9538a4bb623084bbaa
urasefe <162623038+urasefe@users.noreply.github.com>
2024-09-11 20:33:44 +0300

    Update botv2.py

--- botv2.py
+++ botv2.py
@@ -1,12 +1,14 @@
 import discord
 import time
 import random
+import requests
 from discord.ext import commands
-
+from tokenpy import token
+import os
 intents = discord.Intents.default()
 intents.message_content = True
 tkmsonuc = ""
-bot = commands.Bot(command_prefix='$', intents=intents)
+bot = commands.Bot(command_prefix='kuki ', intents=intents)
 liste = ["taş","kağıt","makas"]
 def tkmf(tkmarg):
     global tkmsonuc
@@ -28,7 +30,15 @@ def tkmf(tkmarg):
         tkmsonuc = "Rakibin kağıt dedi kazandın"
     elif tkmarg == "makas" and random.choice(liste) == "makas":
         tkmsonuc = "Rakibin makas dedi berabere"
+ordeklist = []
 
+
+
+def duckhuntimg():
+    url = "https://random-d.uk/api/random"
+    res = requests.get(url)
+    data = res.json()
+    return data["url"]
 @bot.event
 async def on_ready():
     print(f'{bot.user} olarak giriş yaptık')
@@ -37,9 +47,6 @@ async def on_ready():
 async def hello(ctx):
     await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!')
 
-@bot.command()
-async def heh(ctx, count_heh = 5):
-    await ctx.send("he" * count_heh)
 @bot.command()
 async def roll(ctx, dice: str):
     """Rolls a dice in NdN format."""
@@ -62,6 +69,7 @@ async def add(ctx, left: int, right: int):
     await ctx.send(left + right)
 @bot.command()
 async def tkm(ctx, tkmdeger):
+
     await ctx.send("3")
     time.sleep(1)
     await ctx.send("2")
@@ -69,5 +77,43 @@ async def tkm(ctx, tkmdeger):
     await ctx.send("1")
     tkmf(tkmdeger)
     await ctx.send(tkmsonuc)
+
+@bot.command()
+async def mem(ctx):
+    memlist = os.listdir("memler")
+    secilenmem = ""
+    if random.randint(1,100) == 1:
+        secilenmem = "memler\mem4.jpg"
+    else:
+        secilenmem = random.choice(memlist)
     
-bot.run("TOKEN")
+    with open(f'memler/{secilenmem}', 'rb') as f:
+            picture = discord.File(f)
+   # Daha sonra bu dosyayı bir parametre olarak gönderebiliriz!
+    await ctx.send(file=picture)
+
+@bot.command("duckhunt")
+async def duckhunt(ctx, sayibil):
+    image_url = duckhuntimg()
+    await ctx.send(image_url)
+    rs = random.randint(1,10)
+    await ctx.send(rs)
+    if int(sayibil) == rs:
+        await ctx.send("ördeği yakaladın!")
+        ordeklist.append(image_url)
+    else:
+        await ctx.send("yakalayamadın :(")
+@bot.command()
+async def duckscaught(ctx):
+    await ctx.send(len(ordeklist),"ördek yakaladın!")
+
+@bot.command()
+async def helpme(ctx):
+    await ctx.send("Spotify oynatma listem için /playlisy|Github discord örnekler için /dcrep|Ana github repositoryim için /gitmrep|Ördek avı için /duckhunt|Ördek koleksiyonu için /duckscaught|taş kağıt makas için /tkm ,oynanacak şey|Rastgele meme için /mem|")
+@bot.command()
+async def oyun(ctx):
+    os.startfile("https://play.unity.com/en/games/687d91ef-b3bf-4eb0-a451-91a4b2f4c343/webgl-builds")
+
+
+
+bot.run(token)
